f4e59591625354423f628e1f06c9520c027dd4a8
Ali Ghavampour <alighavam79@gmail.com>
2023-06-06 16:14:50 -0400

    added make_scan_tgt

--- makeTGT.py
+++ makeTGT.py
@@ -1,10 +1,11 @@
 # creates target files for the BMW1 experiment
 
 import numpy as np
+import random
 import pandas as pd
 import os
 
-def make_train_tgt(targetPath, sampleSubjName):
+def make_train_tgt(targetPath, sampleSeed):
 
     # Find the last train subject name:
     lastSubj = 'suow01'
@@ -20,14 +21,76 @@ def make_train_tgt(targetPath, sampleSubjName):
     # read sample .tgt file, shuffle and make new .tgt file:
     for file in os.listdir(targetPath):
         if file.endswith('.tgt'):
-            if file[0:12] == sampleSubjName+'_train':
+            if file[0:12] == sampleSeed+'_train':
                 sampleTarget = pd.read_csv(targetPath + '/' + file, sep='\t')
                 sampleTargetShuffled = sampleTarget.sample(frac=1)
                 newTargetName = newSubj + '_' + file[7:]
                 sampleTargetShuffled.to_csv(targetPath + '/' + newTargetName, sep='\t', index=False)
 
+def make_startTime(tStart,nTotal,nRest,iti,itiRest):
+    import numpy as np
+
+    # Set the length of the array
+    length = nTotal
+
+    # Initialize the array with zeros
+    trial_time_array = np.zeros(length, dtype=int)
+
+    # Set the first element
+    trial_time_array[0] = tStart
+
+    # Set the increment for regular elements
+    regular_increment = iti
+
+    # Set the increment for random elements
+    random_increment = itiRest
+
+    # Set the indices for random elements
+    random_indices = np.random.choice(length, nRest, replace=False)
+
+    # Generate the array
+    for i in range(1, length):
+        if i in random_indices:
+            trial_time_array[i] = trial_time_array[i - 1] + random_increment
+        else:
+            trial_time_array[i] = trial_time_array[i - 1] + regular_increment
+
+    return trial_time_array
+
+def make_scan_tgt(targetPath, sampleSeed):
+
+    # Find the last scan subject name:
+    lastSubj = 'suow01' # you don't need to change this. This is just an initial value.
+    for file in os.listdir(targetPath):
+        if file.endswith('.tgt'):
+            if file[7:11] == 'scan':
+                if lastSubj < file[0:6]:
+                    lastSubj = file[0:6]
+    
+    # create new train subject based on the last subject:
+    newSubj = 'suwo{:02d}'.format(int(lastSubj[-2:])+1)            
+
+    # read sample .tgt file, shuffle and make new .tgt file:
+    for file in os.listdir(targetPath):
+        if file.endswith('.tgt'):
+            if file[0:11] == sampleSeed+'_scan':
+                sampleTarget = pd.read_csv(targetPath + '/' + file, sep='\t')
+                sampleTargetShuffled = sampleTarget.sample(frac=1)
+
+                # make startTime vector:
+                startTime = make_startTime(8245,96,9,7055,12070)
+
+                # add the new startTime vector to the dataframe:
+                sampleTargetShuffled.startTime = startTime
+
+                # save the target file:
+                newTargetName = newSubj + '_' + file[7:]
+                sampleTargetShuffled.to_csv(targetPath + '/' + newTargetName, sep='\t', index=False)
+
 
 dir_path = os.getcwd()
-sampleSubjName = 'suwo01'
-sampleTarget = make_train_tgt(dir_path + '/target/bimanualMRuwo_target/', sampleSubjName)
+sampleSeed = 'suwo01'
+# make_train_tgt(dir_path + '/target/bimanualMRuwo_target/', sampleSeed)
+make_scan_tgt(dir_path + '/target/bimanualMRuwo_target/', sampleSeed)
 # print(sampleTarget.u_or_b)
+
