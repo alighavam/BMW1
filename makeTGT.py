6d4a4f137c4e352203de40d6fd94dd9f521cb42f
Ali Ghavampour <alighavam79@gmail.com>
2023-06-09 16:47:54 -0400

    fixed make_startTime()

--- makeTGT.py
+++ makeTGT.py
@@ -1,8 +1,8 @@
 # creates target files for the BMW1 experiment
 
 import numpy as np
-import random
 import pandas as pd
+import random
 import os
 
 def make_train_tgt(targetPath, sampleSeed):
@@ -28,7 +28,6 @@ def make_train_tgt(targetPath, sampleSeed):
                 sampleTargetShuffled.to_csv(targetPath + '/' + newTargetName, sep='\t', index=False)
 
 def make_startTime(tStart,nTotal,nRest,iti,itiRest):
-    import numpy as np
 
     # Set the length of the array
     length = nTotal
@@ -42,16 +41,16 @@ def make_startTime(tStart,nTotal,nRest,iti,itiRest):
     # Set the increment for regular elements
     regular_increment = iti
 
-    # Set the increment for random elements
-    random_increment = itiRest
+    # Set the increment for rest elements
+    rest_increment = itiRest
 
     # Set the indices for random elements
-    random_indices = np.random.choice(length, nRest, replace=False)
+    random_indices = np.random.choice(np.arange(1,length), nRest, replace=False)
 
     # Generate the array
     for i in range(1, length):
         if i in random_indices:
-            trial_time_array[i] = trial_time_array[i - 1] + random_increment
+            trial_time_array[i] = trial_time_array[i - 1] + rest_increment
         else:
             trial_time_array[i] = trial_time_array[i - 1] + regular_increment
 
@@ -90,7 +89,6 @@ def make_scan_tgt(targetPath, sampleSeed):
 
 dir_path = os.getcwd()
 sampleSeed = 'suwo01'
-# make_train_tgt(dir_path + '/target/bimanualMRuwo_target/', sampleSeed)
+make_train_tgt(dir_path + '/target/bimanualMRuwo_target/', sampleSeed)
 make_scan_tgt(dir_path + '/target/bimanualMRuwo_target/', sampleSeed)
-# print(sampleTarget.u_or_b)
 
