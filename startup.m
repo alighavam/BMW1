a34059bf2f126cefe6c153f1095022ecd55d5827
Ali Ghavampour <aghavamp@AFI-CBS-H-1.robarts.ca>
2023-07-11 02:26:16 -0400

    startup.m initial commit

---
+++ startup.m
@@ -0,0 +1,9 @@
+
+if (isunix)
+    addpath(fullfile('/home/ROBARTS/aghavamp/Desktop/matlab','spmj_tools/'))
+    addpath(fullfile('/home/ROBARTS/aghavamp/Desktop/matlab','spm12/'))
+    addpath(strcat(filePath,'functions/'))
+    addpath(genpath(fullfile('/home/ROBARTS/aghavamp/Desktop/matlab','dataframe/')))
+elseif (ismac)
+    
+end
